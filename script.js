6c17e2c3e90d8111bf1615044ea52866f630762d
Vimal Patel <vimal.patel@mountblue.io>
2018-05-21 10:48:05 +0530

    store items with localhost

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-var count = 0;
-var completed = 0;
+var count;
+var completed;
+
+var percentText;
+var percentBar;
+var percentBarFill;
+var list;
+var inputText;
+
+
+function initialize() {
+    count = 0;
+    completed = 0;
+    percentText = document.getElementById("percent-text");
+    percentBar = document.getElementById("percent-bar");
+    percentBarFill = document.getElementById("percent-bar-fill");
+    list = document.getElementById("list");
+    inputText = document.getElementById("input-text");
+
+
+}
 
 
 function updateStats(percentText, percentBar, percentBarFill) {
@@ -23,15 +42,66 @@ function keyUp(event) {
     }
 }
 
-function loaded() {}
+function loaded() {
+    initialize();
+
+    var tempCount;
+    if ((tempCount = localStorage.getItem("count")) == null)
+        tempCount = 0;
+    if ((tempCompleted = localStorage.getItem("completed")) == null)
+        tempCompleted = 0;
+    tempCount = Number(tempCount);
+    tempCompleted = Number(tempCompleted);
+
+    //console.log("1: "+localStorage.getItem("innerList"));
+    if ((innerList = localStorage.getItem("innerList")) == null){
+        innerList = "";
+        return;
+    }
+    if (innerList.trim() == ""){
+        return;
+    }
+    //console.log("2 "+innerList);
+
+    var tempList = document.createElement('div');
+    var body = document.getElementById("body");
+    body.appendChild(tempList);
+    tempList.innerHTML = innerList;
+    console.log(tempList.innerHTML);
+    
+    var i = 0;
+    var listOffspring = tempList.firstChild;
+    console.log("check?");
+
+    console.log(listOffspring.innerHTML);
+    while (i<tempCount) {
+        inputText.value = listOffspring.getElementsByClassName("para")[0].innerHTML; 
+        //console.log(inputText.value); 
+        console.log(i);
+        addToDo();
+        listOffspring = listOffspring.nextSibling;
+        i = i+1;
+    }
+
+    body.removeChild(tempList);
+
+
+    console.log(tempCount);
+    console.log(tempCompleted);
+    console.log(innerList);
+}
+
+function unloading() {
+
+  localStorage.setItem("count", count);
+  localStorage.setItem("completed", completed);
+  localStorage.setItem("innerList", list.innerHTML);
+  list = document.getElementById("list");
+
+
+}
 
 function addToDo() {
-
-    percentText = document.getElementById("percent-text");
-    percentBar = document.getElementById("percent-bar");
-    percentBarFill = document.getElementById("percent-bar-fill");
-    list = document.getElementById("list");
-    inputText = document.getElementById("input-text");
 
 
     //return if task is empty
@@ -56,9 +126,11 @@ function addToDo() {
         if (checkbox.checked == true) {
             completed++;
             newItem.style.textDecoration = "line-through";
+            checkbox.classList.add("checked");
         } else {
             completed--;
             newItem.style.textDecoration = "none";
+            checkbox.classList.remove("checked");
         }
 
         updateStats(percentText, percentBar, percentBarFill);
@@ -109,13 +181,19 @@ function addToDo() {
     list.appendChild(newItem);
 
 
-    // handle hover over to-do 
-    newItem.onmouseover = function () {
-        deleteButton.style.visibility = "visible";
-    }
-    newItem.onmouseout = function () {
-        deleteButton.style.visibility = "hidden";
-    }
+     //handle hover over to-do 
+     newItem.onmouseover = function () {
+         deleteButton.style.visibility = "visible";
+     }
+     newItem.onmouseout = function () {
+         deleteButton.style.visibility = "hidden";
+     }
+    // newItem.addEventListener("mouseover", whenMouseOver);
+
+    // function whenMouseOver(event) {
+    //     deleteButton.style.visibility = "visible";
+    // }
+
 
     //update stats UI, etc
     count++;
